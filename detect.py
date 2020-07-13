fc8292d4fbdf1faf79b09ae0bc8332d4d5bf0a35
Arsh <arshagarwal.18@gmail.com>
2020-07-13 07:11:13 +0530

    modified draw_boxes

--- detect.py
+++ detect.py
@@ -12,12 +12,14 @@ import utils
 from options import options
 import os
 
-def draw_boxes(image,detection_result,class_names,file_name):
+def draw_boxes(detection_result,class_names,file_name):
     """ 
     image= image
     detection_result=dictionary of class to [box_cen_x,box_cen_y,box_width,box_height,confidence]
     class_names=array of class_names
     """
+    source_img = Image.open(file_name)
+    source_img=source_img.resize((416,416))
     for i in detection_result.keys():
         if(len(detection_result[i])==0):
             continue
@@ -26,17 +28,19 @@ def draw_boxes(image,detection_result,class_names,file_name):
         box_cen_y=detection_result[i][0][1]
         box_width=detection_result[i][0][2]
         box_height=detection_result[i][0][3]
-        source_img = Image.open(file_name)
+        
         draw = ImageDraw.Draw(source_img)
         draw.rectangle(((box_cen_x,box_cen_y ), (box_height, box_width)))
         font = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
         draw.text((box_cen_x, box_cen_y),label,font=font ,fill=(255,255,255,128) )
      
-    source_img.save('results/'+file_name.split('/')[-1])
+    source_img.save('results/'+file_name.split('/')[-1],'JPEG')
 
 file_name="coco.names"
 classes_names = utils.load_class_names('./data/labels/coco.names')
-os.mkdir('results')
+
+if(('results' in os.listdir(os.getcwd()))== False):
+   os.mkdir('results')
 
 options=options()()
 data_path=options.data_path
@@ -49,6 +53,7 @@ for i in image_names:
     #detection_result=detection_result[0]
     # draws boxes and saves the results in results folder
     utils.draw_boxes([file],detection_result,classes_names,(416,416))
+    #draw_boxes(detection_result[0],classes_names,file)
 
 
 
--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def load_class_names(file_name):
         class_names = f.read().splitlines()
     return class_names
 
-def draw_boxes(img_names, boxes_dicts, class_names, model_size):
+def draw_boxes(img_names, boxes_dicts, class_names, model_size=(416,416)):
     """Draws detected boxes.
     Args:
         img_names: A list of input images names.
@@ -50,25 +50,22 @@ def draw_boxes(img_names, boxes_dicts, class_names, model_size):
     for num, img_name, boxes_dict in zip(range(len(img_names)), img_names,
                                          boxes_dicts):
         img = Image.open(img_name)
-       
+        img=img.resize((416,416))
         draw = ImageDraw.Draw(img)
         font = ImageFont.truetype(font='./data/fonts/futur.ttf',
                                   size=(img.size[0] + img.size[1]) // 100)
-        resize_factor = \
-            (img.size[0] / model_size[0], img.size[1] / model_size[1])
+       
         for cls in range(len(class_names)):
             boxes = boxes_dict[cls]
             if np.size(boxes) != 0:
                 color = colors[cls]
                 for box in boxes:
                     xy, confidence = box[:4], box[4]
-                    xy = [xy[i] * resize_factor[i % 2] for i in range(4)]
+                    
                     x0, y0 = xy[0], xy[1]
-                    thickness = (img.size[0] + img.size[1]) // 200
-                    for t in np.linspace(0, 1, thickness):
-                        xy[0], xy[1] = xy[0] + t, xy[1] + t
-                        xy[2], xy[3] = xy[2] - t, xy[3] - t
-                        draw.rectangle(xy, outline=tuple(color))
+                    
+                        
+                    draw.rectangle(xy, outline=tuple(color))
                     text = '{} {:.1f}%'.format(class_names[cls],
                                                confidence * 100)
                     text_size = draw.textsize(text, font=font)
