3dba6acc4140c0986dc19963f3b0d8580dcce811
arshagarwal <31827288+arshagarwal@users.noreply.github.com>
2020-07-23 02:08:48 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 pip uninstall tensorflow
 pip install tensorflow=={1.14}
-git clone https://github.com/arshagarwal/Yolo_object_detection.git
 cd /content/Yolo_object_detection
 wget -P weights https://pjreddie.com/media/files/yolov3.weights
 python detect.py $1
